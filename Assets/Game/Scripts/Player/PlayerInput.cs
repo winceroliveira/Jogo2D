0bcce1ac5ddb3b0c6b01b48d9e8e76fd1c6254cd
Wincer Oliveira <85763225+winceroliveira@users.noreply.github.com>
2022-06-16 18:20:26 -0300

    Moviments Mobile Implemented

--- Assets/Game/Scripts/Player/PlayerInput.cs
+++ Assets/Game/Scripts/Player/PlayerInput.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerInput : MonoBehaviour
 {
+    private struct PlayerInputConstants
+    {
+        public const string Horizontal = "Horizontal";
+        public const string Vertical = "Vertical";
+        public const string Jump = "Jump";
+    }
    public Vector2 GetMovementInput()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //input Teclado
+        float horizontalInput = Input.GetAxisRaw(PlayerInputConstants.Horizontal);
+
+        //input mobile
+        if (Mathf.Approximately(horizontalInput,0.0f))
+        {
+            horizontalInput = CrossPlatformInputManager.GetAxisRaw(PlayerInputConstants.Horizontal);
+        }
         return new Vector2(horizontalInput,0);
     }
 
     public bool IsJumpButtonDown()
     {
         //retorna verdadeiro somente na primeira vez que aperta espaço
-        return Input.GetKeyDown(KeyCode.Space);
+        bool isKeyboardButtonDown = Input.GetKeyDown(KeyCode.Space);
+        bool isMobileButtonDown = CrossPlatformInputManager.GetButtonDown(PlayerInputConstants.Jump);
+        return isKeyboardButtonDown || isMobileButtonDown;
     }
     public bool IsJumpButtonHeld()
     {
         //retorna verdadeiro enquanto estiver apertando o espaço
-        return Input.GetKey(KeyCode.Space);
+        bool isKeyboardButtonHeld = Input.GetKey(KeyCode.Space);
+        bool isMobileButtonHeld = CrossPlatformInputManager.GetButton(PlayerInputConstants.Jump);
+        return isKeyboardButtonHeld || isMobileButtonHeld;
     }
     public bool IsCrouchButtonDown()
     {
-        return Input.GetKeyDown(KeyCode.S);
+        bool isKeyboradButtonDown = Input.GetKeyDown(KeyCode.S);
+        bool isMobileButtonDown = CrossPlatformInputManager.GetAxisRaw(PlayerInputConstants.Vertical) < 0;
+        return isKeyboradButtonDown || isMobileButtonDown;
     }
     public bool IsCrouchButtonUp()
     {
-        return Input.GetKeyUp(KeyCode.S);
+        bool isKeyboradButtonUp = Input.GetKey(KeyCode.S) == false;
+        bool isMobileButtonUp = CrossPlatformInputManager.GetAxisRaw(PlayerInputConstants.Vertical) >= 0;
+        return isKeyboradButtonUp && isMobileButtonUp;
     }
 }
