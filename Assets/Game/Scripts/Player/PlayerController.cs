b5695cdf1bfb6f6b003b3e8e09a30a078ccb7bc8
Wincer Oliveira <85763225+winceroliveira@users.noreply.github.com>
2022-06-18 09:53:54 -0300

    AnimationMoviment Implemented

--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -12,8 +12,15 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer spriteRenderer;
     PlayerInput playerInput;
 
-    public Sprite crouchedSprit;
-    public Sprite idleSprit;
+    [Header ("Camera")]
+    public Transform cameraTarget;
+    [Range(0.0f,5.0f)]
+    public float cameraTargetOffsetX = 2.0f;
+    [Range(0.5f,50.0f)]
+    public float cameraTargetFlipSpeed = 2.0f;
+    [Range(0.0f,5.0f)]
+    public float characterSpeedInfluence = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -52,17 +59,21 @@ public class PlayerController : MonoBehaviour
         if (playerInput.IsCrouchButtonDown())
         {
             playerMovement.Crouch();
-
-            //TODO: Remover quando adicionar animação
-            spriteRenderer.sprite = crouchedSprit;
         }
         //LEVANTAR
         else if (playerInput.IsCrouchButtonUp())
         {
-
             playerMovement.UnCrouch();
-            //TODO: Remover quando adicionar animação
-            spriteRenderer.sprite = idleSprit;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        //controle do target da camera dependendo da direção do sprite e da velocidade do jogador
+        bool isFacingRight = spriteRenderer.flipX == false;
+        float targetOffsetX = isFacingRight ? cameraTargetOffsetX : -cameraTargetOffsetX;
+        float currentOffsetX = Mathf.Lerp(cameraTarget.localPosition.x, targetOffsetX, Time.fixedDeltaTime * cameraTargetFlipSpeed);
+        currentOffsetX += playerMovement.CurrentVelocity.x * Time.fixedDeltaTime * characterSpeedInfluence;
+        cameraTarget.localPosition = new Vector3(currentOffsetX, cameraTarget.localPosition.y, cameraTarget.localPosition.z);
     }
 }
